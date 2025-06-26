98d368b93f0c8ea476f1ec3f7c2d1f3815c1a523
Shystra <jlsp.2019@hotmail.com>
2025-06-25 21:11:55 -0300

    first commit: add db local e classes de tarefas e status;

---
+++ TarefeiroAPI/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TarefeiroAPI.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<TarefaTipo> TarefaTipos { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
