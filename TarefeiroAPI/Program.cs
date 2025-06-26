56f19fffbb722b9d095396f16d7ebb6b6f7d2184
Shystra <jlsp.2019@hotmail.com>
2025-06-25 21:53:04 -0300

    feat: adiciona estrutura cors de conexão para front.

--- TarefeiroAPI/Program.cs
+++ TarefeiroAPI/Program.cs
@@ -1,6 +1,8 @@
 using TarefeiroAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
+var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,7 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Tarefeiro API",
+        Version = "v1"
+    });
+});
+
 
 builder.Services.AddDbContext<DataContext>(
     options =>
@@ -17,16 +27,32 @@ builder.Services.AddDbContext<DataContext>(
     }
  );
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tarefeiro API v1");
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseAuthorization();
 
